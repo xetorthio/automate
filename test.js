edaf7a9ac02f59bd152fa9926e6be50931e80d1b
Luis Dario Simonassi <ldsimonassi@gmail.com>
2011-03-19 21:02:26 -0300

    Adapted automate.js to test using node.js

---
+++ test.js
@@ -0,0 +1,24 @@
+var lib=require('./automate.js');
+
+lib.AutoMate.EC2.build(function(){
+
+  var sg = securityGroup("InstanceSecurityGroup", "Enable SSH access and HTTP access on the inbound port", function() {
+    tcp(22, 22, "0.0.0.0/0");
+  });
+
+  var i1 = instance("Ec2Instance1", {
+    securityGroups:[sg],
+    ami:"ami-7a11e213",
+    userData:{}
+  });
+
+  var i2 = instance("Ec2Instance2", {
+    securityGroups:[sg],
+    ami:"ami-7a11e213",
+    userData:{}
+  });
+
+  var lb = loadbalancer("ElasticLoadBalancer", {
+    instances:[i1,i2]
+  });
+});
