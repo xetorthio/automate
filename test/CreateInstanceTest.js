39f4339bf6ed8cda7c05b2b40e8aaeece353c8c0
Jonathan Leibiusky <ionathan@gmail.com>
2011-04-03 19:22:59 -0300

    add missing tests

---
+++ test/CreateInstanceTest.js
@@ -0,0 +1,24 @@
+AutoMate.EC2.build("simple test", {
+  parameters: function() { 
+      string("WebServerPort", "port of the web server", "8888");
+      string("KeyName", "key name");
+  },
+      
+  resources: function(params) {
+    var defaultSG = securityGroup("defaultSG", "default sg", function() {
+      tcp(22, 22, "0.0.0.0/0");
+      tcp(params.WebServerPort, params.WebServerPort, "0.0.0.0/0");
+    });
+
+
+    var ws = instance("ws", {
+      securityGroups:[defaultSG],
+      ami:"ami-7a11e213",
+      instanceType: "t1.micro",
+      keyName: params.KeyName,	
+      userData: ""
+    });
+    
+    var eip= elasticIp("EIP", ws);
+  }
+});
