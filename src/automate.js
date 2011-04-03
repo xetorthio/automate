8df9aca47442b8f731726acf7f1c57dac6f88d7d
Luis Dario Simonassi <ldsimonassi@gmail.com>
2011-04-03 15:43:17 -0300

    Tests run.

--- src/automate.js
+++ src/automate.js
@@ -1,22 +1,12 @@
+function unRef(parameter) {
+	return parameter.reference?parameter.reference():parameter;
+}
 
 var AutoMate= {
 	EC2: {
-		build: function(dsl) {	
-		  var model = {"AWSTemplateFormatVersion": "2010-09-09",  "Description" : "This is a test", "Resources" : {},
-				"Parameters" : {
-				   "WebServerPort" : {
-					 "Description" : "TCP/IP port of the web server",
-					 "Type" : "String",
-					 "Default" : "8888"
-				   },
-				   "KeyName" : {
-					 "Description" : "Name of an existing EC2 KeyPair to enable SSH access to the instance",
-					 "Type" : "String"
-				   }
-				 },
-			};
+		build: function(description, dsl) {	
 
-		  
+		  var model = {"AWSTemplateFormatVersion": "2010-09-09",  "Description" : description, "Resources" : {}, "Parameters" : {} };
 		  
 		  Object.prototype.instance = function(name, data) {
 
@@ -24,20 +14,23 @@ var AutoMate= {
 			  "Type":"AWS::EC2::Instance",
 			  "Properties": {
 					"SecurityGroups" : [ ],
-					"KeyName" : { Ref: "KeyName" },
-					"ImageId" : "ami-7a11e213",
-					"InstanceType" : "t1.micro",
-					"UserData" : { "Fn::Base64" : { "Ref" : "WebServerPort" }}
+					"KeyName" : unRef(data.key_name),
+					"ImageId" : unRef(data.ami),
+					"InstanceType" : unRef(data.instance_type),
+					"UserData" : { "Fn::Base64" : { "Ref" : "WebServerPort" }} //TODO: Implement
 			  }
 			}
 
 			for(sg in data.securityGroups) {
 			  model.Resources[name].Properties.SecurityGroups.push({"Ref":sg.name});
 			}
+
+			model.Resources[name].reference= function() {
+                return {"Ref":name};
+            }
+
 			return {"name":name,"data":data};
 		  }
-		  
-		  
 		  
 		  Object.prototype.securityGroup = function(name, description, rules) {
 			model.Resources[name] = {
@@ -56,7 +49,13 @@ var AutoMate= {
 				"CidrIp" : ip
 			  });
 			}
+
 			rules();
+
+			model.Resources[name].reference= function() {
+                return {"Ref":name};
+            }
+
 			return {"name":name, "description":description, "rules":rules};
 		  }
 		  
@@ -71,9 +70,41 @@ var AutoMate= {
 			for(ins in data.instances) {
 			  model.Resources[name].Properties.Instances.push({"Ref":ins.name});
 			}
+
+			model.Resources[name].reference= function() {
+                return {"Ref":name};
+            }
+
 			return {"name":name,"data":data};
 		  }
-		  dsl();
+
+          Object.prototype.elasticIp = function(name, resource) {
+			model.Resources[name]= {};
+			model.Resources[name].Type= "AWS::EC2::EIP";
+			model.Resources[name].Properties= {};
+			model.Resources[name].Properties.InstanceId= unRef(resource);
+
+			model.Resources[name].reference= function() {
+                return {"Ref":name};
+            }
+			return model.Resources[name];
+		  }
+
+		  Object.prototype.string = function(name, description, defaultValue) {
+			model.Parameters[name]= {};
+			model.Parameters[name].Description= description;
+			model.Parameters[name].Type= "String";
+			if(defaultValue) 
+				model.Parameters[name].Default= defaultValue;
+
+			model.Parameters[name].reference= function() {
+				return {"Ref":name};
+			}
+		  }
+
+		  dsl.parameters();
+		  dsl.resources(model.Parameters);
+
 		  return JSON.stringify(model);
 		}
 	}
--- test/test.js
+++ test/test.js
@@ -1,51 +1,49 @@
 var lib = require('../src/automate.js'),
-    assert = require('assert'),
-    fs = require('fs');
+assert = require('assert'),
+fs = require('fs');
 
-var json = lib.AutoMate.EC2.build(function(){
-var json = AutoMate.EC2.build({
-	parameters: function() { 
-		string("WebServerPort", "TCP/IP port of the web server", "8888");
-		string("KeyName", "Name of an existing EC2 KeyPair to enable SSH access to the instance");
-	},
-	
-	resources: function(params) {
-	  var sg = securityGroup("InstanceSecurityGroup", "Enable SSH access and HTTP access on the inbound port", function() {
-		tcp(22, 22, "0.0.0.0/0");
-	  });
-	
-	  var i1 = instance("Ec2Instance1", {
-		securityGroups:[sg],
-		ami:"ami-7a11e213",
-		type:"t1.micro",
-		key_name: params.KeyName,
-		user_data: 
-	  });
-	
-	  var i2 = instance("Ec2Instance2", {
-		securityGroups:[sg],
-		ami:"ami-7a11e213",
-		userData:{}
-	  });
-	
-	  var lb = loadbalancer("ElasticLoadBalancer", {
-		instances:[i1,i2]
+
+var json = lib.AutoMate.EC2.build("1st test", {
+        parameters: function() { 
+            //string("WebServerPort", "TCP/IP port of the web server", "8888");
+            string("KeyName", "Name of an existing EC2 KeyPair to enable SSH access to the instance");
+        },
+        
+        resources: function(params) {
+          var sg = securityGroup("InstanceSecurityGroup", "Enable SSH access and HTTP access on the inbound port", function() {
+            tcp(22, 22, "0.0.0.0/0");
+          });
+
+          var i1 = instance("Ec2Instance1", {
+            securityGroups:[sg],
+            ami:"ami-7a11e213",
+            instance_type: "t1.micro",
+            key_name: params.KeyName,	
+            user_data: ""
+          });
+        
+          var i2 = instance("Ec2Instance2", {
+            securityGroups:[sg],
+            ami:"ami-7a11e213",
+            instance_type: "t1.micro",
+            key_name: params.KeyName,	
+            userData: ""
+          });
+        
+          var lb = loadbalancer("ElasticLoadBalancer", {
+            instances:[i1,i2]
+          });
+          
+          var eip= elasticIp("EIP", i2);
+        }
 	  }
-	  
-	  var eip= elasticIp("EIP", i2);
-	  
-	},
-	
-	output: function(param, resources) {
-		add("URL", "URL of the sample website", ref(resources.EIP.InstanceId));
-		add("PUBLIC", "Public dns", getAttr(resources.Ec2Instance2, "PublicDns"));
-	}
- 
-	);
-});
+    );
+
 
 var am_template = JSON.parse(json);
 var template = JSON.parse(fs.readFileSync("EC2WebSiteSample-1.0.0.template", 'utf8'));
+
+fs.writeFileSync("./output.log", am_template, 'utf8')
 
 console.log("Generated JSON:\n"+json+"\n");
 
